5e012fdb228c6272f59f2b9b672e01cbbc831d9d
19aw0107 <19aw0107@gmail.com>
2020-09-14 01:07:12 +0900

    14st

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <footer>
     <span class="footer-p">© 2020 kadekawa koutaro</span>
@@ -27,33 +26,3 @@ footer{
   line-height: 20px;
 }
 </style>
-=======
-<template>
-  <footer>
-    <span class="footer-p">© 2020 kadekawa koutaro</span>
-  </footer>
-</template>
-
-<script>
-export default {
-  /*name: 'HelloWorld',
-  props: {
-    msg: String
-  }*/
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-footer{
-  height: 110px;
-  text-align: center;
-  margin-top: 140px;
-}
-.footer-p{
-  font-size: 14px;
-  font-weight: 500;
-  line-height: 20px;
-}
-</style>
->>>>>>> 87760e7238fc516bc80404edb222c6ebaf6ac068
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <div></div>
 </template>
@@ -16,22 +15,3 @@ export default {
 <style scoped lang="scss">
 
 </style>
-=======
-<template>
-  <div></div>
-</template>
-
-<script>
-export default {
-  /*name: 'HelloWorld',
-  props: {
-    msg: String
-  }*/
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-
-</style>
->>>>>>> 87760e7238fc516bc80404edb222c6ebaf6ac068
